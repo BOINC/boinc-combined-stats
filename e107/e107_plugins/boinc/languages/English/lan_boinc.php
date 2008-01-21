32e0b08b4ec7cd016ad99120eece4faeb30339c6
James E. Drews <drews@engr.wisc.edu>
2008-01-21 19:33:37 +0000

    -Made stats web pages translateable
    git-svn-id: http://boinc.berkeley.edu/svn/trunk/boinc_stats/boinc_combined_stats@14609

---
+++ e107/e107_plugins/boinc/languages/English/lan_boinc.php
@@ -0,0 +1,198 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     BOINC Combined Statistics e107 website system - Language File.
+|     Version 1.00
+|     Date: 2008/01/21 
++----------------------------------------------------------------------------+
+*/
+define("e_PAGETITLE", "BOINC");
+
+/* bp_home.php  and country_rac.php */	
+define("LAN_100", "World BOINC Activity");
+define("LAN_101", "This map shows the current global BOINC activity in GigaFLOPS. The number shown when you mouse over a country is the current GigaFLOPS produced by BOINC participents of that country.  You can zoom in on the map by holding down the left mouse button and draging the mouse over the area you wish to zoom in to.");
+define("LAN_102", "Key");
+define("LAN_103", "Values are in GigaFLOPS");
+define("LAN_104", "No data");
+define("LAN_105", "1");
+define("LAN_106", "1 - 50");
+define("LAN_107", "51 - 100");
+define("LAN_108", "101 - 500");
+define("LAN_109", "501 - 1,000");
+define("LAN_110", "1,001 - 5,000");
+define("LAN_111", "5,001 - 10,000");
+define("LAN_112", "10,001 - 20,000");
+define("LAN_113", "20,001 - 50,000");
+define("LAN_114", "50,001 - 100,000");
+define("LAN_115", "100,001+");
+
+/* bp_retired.php */
+define("LAN_120", "Summary of retired projects");
+
+/* bp_summary.php */
+define("LAN_130", "Summary of projects");
+
+/* bp_menu.php */
+define("LAN_140", "BOINC Project Statistics");
+define("LAN_141", "Can't connect to DB");
+define("LAN_142", "DB Error");
+
+/* bp.php */
+define("LAN_150", " Statistics");
+
+/* u_rank.php */
+define("LAN_160", "Users currently doing the most work");
+define("LAN_161", "Users who have done the most work");
+define("LAN_162", "Users who have done the most work on a single computer");
+define("LAN_163", "Users who are currently doing the most work on a single computer");
+define("LAN_164", "Users who have done the most work on 5 or fewer computers");
+define("LAN_165", "Users who are currently doing the most work on 5 or fewer computers");
+define("LAN_166", "Users who have done the most work on 10 or fewer computers");
+define("LAN_167", "Users who are currently doing the most work on 10 or fewer computers");
+define("LAN_168", "Users who have done the most work on 20 or fewer computers");
+define("LAN_169", "Users who are currently doing the most work on 20 or fewer computers");
+define("LAN_170", "Users who have done the most work and actively participating in 2 or more projects");
+define("LAN_171", "Users who are currently doing the most work and are actively participating in 2 or more projects");
+define("LAN_172", "Users who have done the most work on a single computer and are actively participating in 2 or more projects");
+define("LAN_173", "Users who have done the most work on 5 or fewer computers and are actively participating in 2 or more projects");
+define("LAN_174", "Users who have done the most work on 10 or fewer computers and are actively participating in 2 or more projects");
+define("LAN_175", "Users who have done the most work on 20 or fewer computers and are actively participating in 2 or more projects");
+define("LAN_176", "Users who have done the most work and actively participating in 5 or more projects");
+define("LAN_177", "Users who have done the most work on a single computer and are actively participating in 5 or more projects");
+define("LAN_178", "Users who have done the most work on 5 or fewer computers and are actively participating in 5 or more projects");
+define("LAN_179", "Users who have done the most work on 10 or fewer computers and are actively participating in 5 or more projects");
+define("LAN_180", "Users who have done the most work on 20 or fewer computers and are actively participating in 5 or more projects");
+define("LAN_181", "Users who are currently doing the most work on a single computer and are actively participating in 2 or more projects");
+define("LAN_182", "Users who are currently doing the most work on 5 or fewer computers and are actively participating in 2 or more projects");
+define("LAN_183", "Users who are currently doing the most work on 10 or fewer computers and are actively participating in 2 or more projects");
+define("LAN_184", "Users who are currently doing the most work on 20 or fewer computers and are actively participating in 2 or more projects");
+define("LAN_185", "Users who are currently doing the most work and actively participating in 5 or more projects");
+define("LAN_186", "Users who are currently doing the most work on a single computer and are actively participating in 2 or more projects");
+define("LAN_187", "Users who are currently doing the most work on 5 or fewer computers and are actively participating in 5 or more projects");
+define("LAN_188", "Users who are currently doing the most work on 10 or fewer computers and are actively participating in 5 or more projects");
+define("LAN_189", "Users who are currently doing the most work on 20 or fewer computers and are actively participating in 5 or more projects");
+
+
+/* project_graph.php */
+/* Note: graphs are stuck on English for the moment, these are here for future translation */
+define("LAN_200", "User");
+define("LAN_201", "Host");
+define("LAN_202", "Team");
+define("LAN_203", "Country");
+define("LAN_204", "Credit granted per day");
+define("LAN_205", "Active User");
+define("LAN_206", "Active Host");
+define("LAN_207", "Active Team");
+define("LAN_208", "counts for");
+define("LAN_209", "for");
+
+
+/* user_graph.php */
+/* Note: graphs are stuck on English for the moment, these are here for future translation */
+define("LAN_210", "Failed to get start counter");
+define("LAN_211", "Total Credit (90 day hist)");
+define("LAN_212", "Total Credit (52 week hist)");
+define("LAN_213", "Credit per week (52 week hist)");
+define("LAN_214", "Credit per day (89 day hist)");
+define("LAN_215", "Total credit distribution");
+define("LAN_216", "Current credit/day distribution");
+
+/* bu_rankselect.php */
+define("LAN_220", "User Rankings");
+
+/* get_user.php */
+define("LAN_230", "<error>cpid not specified</error>");
+define("LAN_231", "BOINC User Statistics");
+define("LAN_232", "Error performing query");
+define("LAN_233", "User Summary");
+define("LAN_234", "Name:");
+define("LAN_235", "User Cross Project ID:");
+define("LAN_236", "Total Credit:");
+define("LAN_237", "Recent Average Credit:");
+define("LAN_238", "Rankings");
+define("LAN_239", "by Total Credit");
+define("LAN_240", "by Recent Average Credit");
+define("LAN_241", "World Rank");
+define("LAN_242", "out of");
+define("LAN_243", "percentile");
+define("LAN_244", "Total Credit Graphs");
+define("LAN_245", "stats graph");
+define("LAN_246", "User Projects");
+define("LAN_247", "Project Name:");
+define("LAN_248", "Project Rank Total Credit:");
+define("LAN_249", "Project Rank Recent Average Credit:");
+define("LAN_250", "Project User ID:");
+define("LAN_251", "Number of Computers:");
+define("LAN_252", "90 Day History");
+define("LAN_253", "52 Week History");
+
+
+/* get_cpcs.php */
+define("LAN_260", "Granted credit comparison");
+define("LAN_261", "This chart shows the ratio of credit granted per cpu second for hosts actively participating in any two given projects.  If a value in Row A and Column B is greater than 1, that indicates that project A is awarding more credit per cpu second than project B.  The number in () indicates the number of active hosts participating in both project A and B.");
+define("LAN_262", "Project granted credit comparison");
+
+
+/* bp_search.php */
+define("LAN_270", "Input Error");
+define("LAN_271", "No user specified to search for");
+define("LAN_272", "BOINC User Search");
+define("LAN_273", "User Name");
+define("LAN_274", "Project Name");
+define("LAN_275", "Project Rank");
+define("LAN_276", "User CPID");
+define("LAN_277", "User Search Results");
+
+
+/* bp_functions.php */
+define("LAN_300", "Project Summary");
+define("LAN_301", "Project Name");
+define("LAN_302", "Project URL");
+define("LAN_303", "Development Stage");
+define("LAN_304", "Project for Profit");
+define("LAN_305", "Total Granted Credit");
+define("LAN_306", "Recent Average Credit");
+define("LAN_307", "Total");
+define("LAN_308", "Active");
+define("LAN_309", "User Count");
+define("LAN_310", "Team Count");
+define("LAN_311", "Host Count");
+define("LAN_312", "Granted Credit History");
+define("LAN_313", "User Count History");
+define("LAN_314", "Team Count History");
+define("LAN_315", "Host Count History");
+define("LAN_316", "Project");
+define("LAN_317", "Total/Active Users");
+define("LAN_318", "Total/Active Hosts");
+define("LAN_319", "Total/Active Teams");
+define("LAN_320", "Countries");
+define("LAN_321", "Total Credit");
+define("LAN_322", "XML Data Time (CDT)");
+define("LAN_323", "Total Users");
+define("LAN_324", "Total Hosts");
+define("LAN_325", "Total Teams");
+define("LAN_326", "Computer Count");
+define("LAN_327", "Number of projects actively participating in");
+define("LAN_328", "1 or more (all users)");
+define("LAN_329", "2 or more");
+define("LAN_330", "5 or more");
+define("LAN_331", "By Total Work Done");
+define("LAN_332", "By Recent Work Done");
+define("LAN_333", "All Users");
+define("LAN_334", "View");
+define("LAN_335", "Just 1");
+define("LAN_336", "5 or fewer");
+define("LAN_337", "10 or fewer");
+define("LAN_338", "20 or fewer");
+define("LAN_339", "Select new list");
+define("LAN_340", "Rank");
+define("LAN_341", "Name");
+define("LAN_342", "Total Credit");
+define("LAN_343", "Recent Average Credit");
+define("LAN_344", "Active Projects");
+define("LAN_345", "Total Projects");
+
+
+
+
+?>
